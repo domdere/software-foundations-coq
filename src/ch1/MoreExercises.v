5450c07cdae8cbca34737e4329d2f615c7a01c06
Dom De Re <domdere@domdere.com>
2014-03-15 18:25:39 +1100

    First two exercises work

--- src/ch1/MoreExercises.v
+++ src/ch1/MoreExercises.v
@@ -6,16 +6,24 @@ Require Import ch1.CaseAnalysis.
 (* Use the tactics you have learned so far to prove the following theorem about boolean functions. *)
 
 Theorem identity_fn_applied_twice :
-    forall(f : bool -> bool),
+    forall (f : bool -> bool),
     (forall (x : bool), f x = x) ->
     forall (b : bool), f (f b) = b.
 Proof.
-    intros f.
-    intros H.
-    split H as [x' H2].
-    intros b. destruct b.
-    
+    intros f H b.
+    rewrite -> H.
+    rewrite -> H.
+    reflexivity. Qed.
 
 (* Now state and prove a theorem negation_fn_applied_twice similar to the previous one but where the second hypothesis says that the function f has the property that f x = negb x. *)
 
-(* FILL IN HERE *)
+Theorem negation_fn_applied_twice :
+    forall (f : bool -> bool),
+    (forall (x : bool), f x = negb x) ->
+    forall (b : bool), f (f b) = b.
+Proof.
+    intros f H b.
+    rewrite -> H.
+    rewrite -> H.
+    rewrite -> negb_involutive.
+    reflexivity. Qed.
