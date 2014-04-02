42a1749cdc852255e7b127e81cbb4f323203ba6f
Dom De Re <domdere@domdere.com>
2014-04-02 23:09:20 +1100

    Added a proof by induction

---
+++ src/ch1/Induction.v
@@ -0,0 +1,34 @@
+(* [Case] is not built into Coq: we need to define it ourselves.
+    There is no need to understand how it works -- you can just skip
+    over the definition to the example that follows.  It uses some
+    facilities of Coq that we have not discussed -- the string
+    library (just for the concrete syntax of quoted strings) and the
+    [Ltac] command, which allows us to declare custom tactics.  Kudos
+    to Aaron Bohannon for this nice hack! *)
+
+Require String. Open Scope string_scope.
+
+Ltac move_to_top x :=
+  match reverse goal with
+  | H : _ |- _ => try move x after H
+  end.
+
+Tactic Notation "assert_eq" ident(x) constr(v) :=
+  let H := fresh in
+  assert (x = v) as H by reflexivity;
+  clear H.
+
+Tactic Notation "Case_aux" ident(x) constr(name) :=
+  first [
+    set (x := name); move_to_top x
+  | assert_eq x name; move_to_top x
+  | fail 1 "because we are working on a different case" ].
+
+Tactic Notation "Case" constr(name) := Case_aux Case name.
+Tactic Notation "SCase" constr(name) := Case_aux SCase name.
+Tactic Notation "SSCase" constr(name) := Case_aux SSCase name.
+Tactic Notation "SSSCase" constr(name) := Case_aux SSSCase name.
+Tactic Notation "SSSSCase" constr(name) := Case_aux SSSSCase name.
+Tactic Notation "SSSSSCase" constr(name) := Case_aux SSSSSCase name.
+Tactic Notation "SSSSSSCase" constr(name) := Case_aux SSSSSSCase name.
+Tactic Notation "SSSSSSSCase" constr(name) := Case_aux SSSSSSSCase name.
--- src/ch1/MoreExercises.v
+++ src/ch1/MoreExercises.v
@@ -2,6 +2,7 @@ Require Import ch1.EnumeratedTypes.
 Require Import ch1.Simplification.
 Require Import ch1.Rewriting.
 Require Import ch1.CaseAnalysis.
+Require Import ch1.Induction.
 
 (* Use the tactics you have learned so far to prove the following theorem about boolean functions. *)
 
@@ -39,6 +40,67 @@ one more than twice a binary number.
 *)
 
 Inductive bin : Type :=
-    | O : bin
-    | Even : bin -> bin
-    | Odd : bin -> bin.
+    | Even : nat -> bin
+    | Odd : nat -> bin.
+
+(*
+
+(b) Next, write an increment function for binary numbers, and a function to convert binary numbers to unary numbers.
+
+*)
+
+Fixpoint inc_bin (n : bin) : bin :=
+    match n with
+        | Even n'   => Odd n'
+        | Odd n'    => Even (plus n' 1)
+    end.
+
+Definition bin2nat (n : bin) : nat :=
+    match n with
+        | Even n'   => mult n' 2
+        | Odd n'    => plus (mult n' 2) 1
+    end.
+
+Definition evenbin (n : bin) : bool :=
+    match n with
+        | Even _    => true
+        | Odd _     => false
+    end.
+
+Definition oddbin (n : bin) : bool := negb (evenbin n).
+
+(*
+
+(c) Write some unit tests for your increment and binary-to-unary
+functions.
+
+Notice that incrementing a binary number and then converting it to
+unary should yield the same result as first converting it to unary
+and then incrementing.
+
+*)
+
+Example test_inc_bin1: (inc_bin (Even 0)) = (Odd 0).
+Proof. reflexivity. Qed.
+
+Theorem dist: forall (n : nat),
+    n * 2 + 1 + 1 = (n + 1) * 2.
+
+Proof.
+    intros n.
+    induction n as [| n'].
+    Case "n = 0". reflexivity.
+    Case "n = S n'".
+    simpl.
+    rewrite -> IHn'.
+    reflexivity . Qed.
+
+Theorem inc_bin2nat_commute: forall (n : bin),
+    bin2nat (inc_bin n) = plus (bin2nat n) 1.
+
+Proof.
+    intros n. destruct n as [evenn | oddn].
+    reflexivity.
+    simpl.
+    rewrite -> dist.
+    reflexivity. Qed.
